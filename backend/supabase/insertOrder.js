d2517fedd8a39a69ce3283a03d5b35ed4817560b
Paulo <paulo3252@myyahoo.com>
2025-10-30 10:32:59 -0700

    data collection

--- backend/supabase/insertOrder.js
+++ backend/supabase/insertOrder.js
@@ -1,11 +1,27 @@
 import { getSupabaseClient } from './client.js';
 const supabase = getSupabaseClient();
 
-export async function insertOrder({ product_name, status, email, stripe_session_id }) {
+export async function insertOrder({
+  product_name,
+  status,
+  email,
+  stripe_session_id,
+  shipping_name,
+  shipping_address,
+}) {
   const { data, error } = await supabase
     .from('orders')
-    .insert([{ product_name, status, email, stripe_session_id }]);
+    .insert([
+      {
+        product_name,
+        status,
+        email,
+        stripe_session_id,
+        shipping_name,
+        shipping_address,
+      },
+    ]);
 
-  if (error) console.error('❌ Supabase insert error:', error);
+  if (error) console.error('❌ Supabase insert error:', error.message);
   return data;
 }
